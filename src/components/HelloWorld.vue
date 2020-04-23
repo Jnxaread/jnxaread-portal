51e57f0ce5a3044b3dc79b14c1fb8879e5e5b8d7
李嵩 <lisong@pansoft.com>
2020-04-23 18:50:01 +0800

    安装sass预处理器模块

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,8 +42,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-h3
+<style scoped>
+/*h3
   margin 40px 0 0
 
 ul
@@ -55,5 +55,5 @@ li
   margin 0 10px
 
 a
-  color #42b983
+  color #42b983*/
 </style>
