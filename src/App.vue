24c3b0f1c3bcf72052263d653643b6c6fa7dce85
李嵩 <lisong@pansoft.com>
2020-04-24 14:25:19 +0800

    主页面页头和页脚设计

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,162 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app" class="app">
+        <div class="header">
+            <div class="logo" :style="{ width: logoWidth + 'px' }">
+                <router-link to="/">
+                    谨下阅读
+                </router-link>
+            </div>
+            <div class="nav_module">
+                <div class="nav_item">文库</div>
+                <div class="nav_item">论坛</div>
+            </div>
+            <div class="nav_user" :style="{ width: logoWidth + 'px' }">
+                <div class="login_register">
+                    <div class="nav_item">登录</div>
+                    <div class="nav_item">注册</div>
+                </div>
+            </div>
+        </div>
+        <div class="main" :style="{ minHeight: minHeight + 'px' }">
+            <router-view/>
+        </div>
+        <div class="footer">
+            <div class="about">
+                <router-link to="/">网站介绍</router-link>
+                |
+                <router-link to="/">联系方式</router-link>
+                |
+                <router-link to="/">帮助与反馈</router-link>
+                |
+                <router-link to="/">侵权与投诉</router-link>
+            </div>
+            <div class="website">Copyright © 2020 谨下阅读</div>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style>
-/*#app*/
-  /*font-family Avenir, Helvetica, Arial, sans-serif*/
-  /*-webkit-font-smoothing antialiased*/
-  /*-moz-osx-font-smoothing grayscale*/
-  /*text-align center*/
-  /*color #2c3e50*/
-  /*margin-top: 60px*/
+<script>
+    export default {
+        data() {
+            return {
+                logoWidth: null,
+                theme: 'dark',
+                minHeight: null,
+                selected: 1,
+            }
+        },
+        mounted() {
+            this.minHeight = document.documentElement.clientHeight - 170;
+            this.logoWidth = (window.innerWidth - 1217) / 2;
+        },
+        computed: {
+            isLogin: function () {
+                return this.$store.getters.isLogin;
+            },
+            user: function () {
+                return this.$store.getters.getUser;
+            }
+        },
+        created: function () {
+            if (this.isMobile()) {
+                window.location.href = "http://inhive.net/mobile";
+            }
+            //在页面加载时读取sessionStorage里的状态信息
+            if (sessionStorage.getItem("store")) {
+                this.$store.replaceState(Object.assign({}, this.$store.state, JSON.parse(sessionStorage.getItem("store"))));
+            }
+
+            //在页面刷新时将vuex里的信息保存到sessionStorage里
+            window.addEventListener("beforeunload", () => {
+                sessionStorage.setItem("store", JSON.stringify(this.$store.state));
+            });
+        },
+        methods: {
+            isMobile() {
+                let flag = navigator.userAgent.match(
+                    /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+                );
+                return flag;
+            },
+            toLogin() {
+                this.$router.push('/toLogin');
+            },
+            toRegister() {
+                this.$router.push('/toRegister');
+            },
+        }
+    }
+</script>
+
+<style lang="scss">
+    .app {
+        background-color: #FFFFFF;
+    }
+
+    .header {
+        width: 100%;
+        height: 60px;
+        background-color: #515a6e;
+    }
+
+    .logo {
+        float: left;
+        line-height: 60px;
+        color: #fff;
+        font-size: 32px;
+        font-weight: bold;
+        font-family: YouYuan;
+        text-align: center;
+    }
+
+    .nav_item {
+        display: inline-block;
+        color: #dcdee2;
+        font-size: 23px;
+        font-family: YouYuan;
+        line-height: 60px;
+        margin: 0 10px;
+    }
+
+    .nav_module {
+        float: left;
+        width: 1200px;
+    }
+
+    .nav_user {
+        float: left;
+    }
+
+    .login_register{
+        float: right;
+        width: 132px;
+        margin-right: 10%;
+    }
+
+    .main {
+        width: 1200px;
+        margin: 0 auto;
+        padding: 10px 0px;
+    }
+
+    .footer {
+        width: 100%;
+        height: 120px;
+        color: #fff;
+        padding-top: 30px;
+        text-align: center;
+        background-color: #515a6e;
+    }
+
+    .about a {
+        color: #fff;
+    }
+
+    .website {
+        margin-top: 15px;
+    }
+
+    .ivu-message {
+        z-index: 22000 !important;
+    }
 </style>
